601821ffe610a04eeeb840d6fd735096f79da032
enebin <46271447+enebin@users.noreply.github.com>
2021-10-06 09:10:25 +0900

    Initial Commit

---
+++ PantoneChips/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PantoneChips
+//
+//  Created by 이영빈 on 2021/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PantoneChips/PantoneChipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantoneChipsApp.swift
+//  PantoneChips
+//
+//  Created by 이영빈 on 2021/10/06.
+//
+
+import SwiftUI
+
+@main
+struct PantoneChipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
